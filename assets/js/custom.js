c1ccc173380091e572f403712f98e91f22bdc6cf
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-11-02 12:13:40 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   setTimeout(() => {
     $('.preloader').fadeOut();
-  }, 2000);
+  }, 3000);
 });
 
 $('.homeslider-wrapper').slick({
@@ -13,9 +13,9 @@ $('.homeslider-wrapper').slick({
   cssEase: 'linear',
   arrows: true,
   prevArrow:
-    "<button type='button' class='slick-prev'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-arrow-up'></i></button>",
   nextArrow:
-    "<button type='button' class='slick-next '><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    "<button type='button' class='slick-next '><i class='fa-solid fa-arrow-down'></i></button>",
 });
 document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', function () {
@@ -32,3 +32,81 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 // DOMContentLoaded  end
+
+$('.newsslider-wrapper').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-arrow-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next '><i class='fa-solid fa-arrow-right'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$('.portfolio-slider-wrapper').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  prevArrow:
+    "<button type='button' class='slick-prev'><i class='fa-solid fa-arrow-left'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next '><i class='fa-solid fa-arrow-right'></i></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
